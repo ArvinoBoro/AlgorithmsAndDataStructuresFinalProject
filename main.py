384957d82e3ca6aea1b93251ec85eb348a81dc1e
ArvinoBoro <arvin.ziba@ontariotechu.net>
2024-07-26 18:08:22 -0400

    Very Abstract Graph Class

---
+++ main.py
@@ -0,0 +1,23 @@
+class Graph: 
+
+    def __init__(self):
+        self._graph = {}
+
+    def best_path_search(start_vertex, end_vertex): 
+        '''Uses Dijkstra's Algorithm to find the shortest path from a source vertex to a destination vertex.'''
+        pass
+
+    def add_vertex(self, vertex_ID, *argv):
+        '''Adds a vertex to the graph. Takes the vertex name and a variable number of adjacency tuples. Each tuple consists of an 
+        adjacent vertex and cost.'''
+        pass
+
+    def delete_vertex(self, vertex_ID):
+        '''Deletes a vertex with the given name and all its incident edges.'''
+        pass
+
+def main():
+   pass
+
+if __name__ == '__main__':
+    main()
