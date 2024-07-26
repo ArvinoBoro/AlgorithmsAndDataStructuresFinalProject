b35b5f5470a77d28fa72b92fb7a27bb51364b31e
ArvinoBoro <arvin.ziba@ontariotechu.net>
2024-07-26 19:31:54 -0400

    Reading a CSV File - Imported the csv module for reading the CSV file of graph adjacencies. - Created the function definition for the add_edge() method. - Created the function definition for the vertex_exists() method.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import csv 
 class Graph: 
-
     def __init__(self):
         self._graph = {}
 
@@ -7,17 +7,40 @@ class Graph:
         '''Uses Dijkstra's Algorithm to find the shortest path from a source vertex to a destination vertex.'''
         pass
 
-    def add_vertex(self, vertex_ID, *argv):
-        '''Adds a vertex to the graph. Takes the vertex name and a variable number of adjacency tuples. Each tuple consists of an 
+    def add_vertex(self, vertex):
+        '''Adds a vertex to the graph. Takes the vertex name and a variable number of adjacency tuples as arguement. Each tuple consists of an 
         adjacent vertex and cost.'''
         pass
+        # self._graph[vertex] = {}
 
-    def delete_vertex(self, vertex_ID):
+    def delete_vertex(self, vertex):
         '''Deletes a vertex with the given name and all its incident edges.'''
         pass
 
+    def add_edge(self, vertex1, vertex2, cost):
+        pass 
+
+
+    def vertex_exists(self, vertex):
+        '''Returns true if a vertex with a given name exists on the graph.'''
+        pass 
+
+
 def main():
-   pass
+
+    map = Graph()
+    with open('adjacencies.csv', newline='') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+
+        for row in csv_reader: 
+            if not map.vertex_exists(row[0]):
+                map.add_vertex(row[0])
+
+            if not map.vertex_exists(row[1]):
+                map.add_vertex(row[1])
+
+            map.add_edge(row[0], row[1], row[2])
+
 
 if __name__ == '__main__':
     main()
